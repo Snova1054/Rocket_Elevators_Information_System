686230d61dd823e889939908ba8a3ea0b793ed3c
coctusmccoy <juliendarac@gmail.com>
2021-10-26 12:58:05 -0400

    one way to quotes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  get 'home/index'
+  root 'home#index'
+  get 'home/quote'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
+

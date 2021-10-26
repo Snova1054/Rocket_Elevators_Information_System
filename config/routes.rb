5d3f57ee4aaed39aecbb1817d39683a200b4c631
coctusmccoy <juliendarac@gmail.com>
2021-10-26 17:31:32 -0400

    link more clean

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   root 'home#index'
-  get 'home/quote'
-  get 'home/commercial'
-  get 'home/residential'
+  get 'quote', to: 'home#quote'
+  get 'commercial', to: 'home#commercial'
+  get 'residential', to: 'home#residential'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
 

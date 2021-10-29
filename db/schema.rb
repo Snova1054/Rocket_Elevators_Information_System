d80dac07c1bd230909a4143c4852295c15aa927d
coctusmccoy <juliendarac@gmail.com>
2021-10-28 20:45:55 -0400

    quote css started

--- db/schema.rb
+++ db/schema.rb
@@ -30,4 +30,14 @@ ActiveRecord::Schema.define(version: 2021_10_28_002040) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
 end

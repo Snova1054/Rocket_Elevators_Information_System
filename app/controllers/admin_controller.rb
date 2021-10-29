565945bb67c8b36c8653b01393dc91913246a84d
Snova1054 <istar.dark@gmail.com>
2021-10-29 19:18:52 -0400

    removed authenticate user from admin

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,2 +1,4 @@
 class AdminController < ApplicationController
+    # before_action :authenticate_user
+    # protect_from_forgery prepend: true
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,3 @@
 class ApplicationController < ActionController::Base
     protect_from_forgery prepend: true
-    before_action :authenticate_user!
 end

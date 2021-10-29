d9eb0ea32f73192aa7281f63283c07e8a2f3bb90
Snova1054 <istar.dark@gmail.com>
2021-10-29 09:17:03 -0400

    adjusted prices' string

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,41 +1,38 @@
 class QuotesController < ApplicationController
 
     def new
-        @quote = Quote.new
-      end
+      @quote = Quote.new
+    end
 
-      def index
-        @quotes = Quote.all
-      end
+    def index
+      @quotes = Quote.all
+    end
 
     def show
-        @quote = Quote.find(params[:id])
-      end
-   
-
-    def create
-        @quote = Quote.new(quote_params)
-       
-        @quote.save
-        redirect_to @quote
+      @quote = Quote.find(params[:id])
     end
-
+   
+    def create
+      @quote = Quote.new(quote_params)
+      
+      @quote.save
+      redirect_to @quote
+    end
 
     def building_type
-        @building_type = :building_type
+      @building_type = :building_type
     end
 
-
     def destroy
-        @quote = Quote.find(params[:id])
-        @quote.destroy
-     
-        redirect_to quotes_path
-      end
+      @quote = Quote.find(params[:id])
+      @quote.destroy
+    
+      redirect_to quotes_path
+    end
        
     private
     def quote_params
-          params.require(:quote).permit(:building_type, :appartement, :floor, :basement, :plan, :business, :parking, :cages, :occupant, :elevatorNeeded, :price, :fees, :totalPrice)
+      params.require(:quote).permit(:building_type, :appartement, :floor, :basement, :plan, :business, :parking, :cages, :occupant, :elevatorNeeded, :price, :fees, :totalPrice)
     end
     
 end

e603099fb5c6abd612f34245af53b7b7d55fa5ec
Snova1054 <istar.dark@gmail.com>
2021-10-29 20:32:09 -0400

    changed rights to enter quotes/x to authenticated users

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,39 +1,39 @@
 class QuotesController < ApplicationController
+  before_action :authenticate_user!, :only => [:show]
+  def new
+    @quote = Quote.new
+  end
 
-    def new
-      @quote = Quote.new
-    end
+  def index
+    @quotes = Quote.all
+  end
 
-    def index
-      @quotes = Quote.all
-    end
+  def show
+    @quote = Quote.find(params[:id])
+  end
+  
+  def create
+    @quote = Quote.new(quote_params)
+    
+    @quote.save
+    redirect_to "#home"
+  end
 
-    def show
-      @quote = Quote.find(params[:id])
-    end
-   
-    def create
-      @quote = Quote.new(quote_params)
+  def building_type
+    @building_type = :building_type
+  end
+
+  def destroy
+    @quote = Quote.find(params[:id])
+    @quote.destroy
+  
+    redirect_to quotes_path
+  end
       
-      @quote.save
-      redirect_to "#home"
-    end
-
-    def building_type
-      @building_type = :building_type
-    end
-
-    def destroy
-      @quote = Quote.find(params[:id])
-      @quote.destroy
-    
-      redirect_to quotes_path
-    end
-       
-    private
-    def quote_params
-      params.require(:quote).permit(:building_type, :appartement, :floor, :basement, :plan, :business, :parking, :cages, :occupant, :elevatorNeeded, :price, :fees, :totalPrice)
-    end
-    
+  private
+  def quote_params
+    params.require(:quote).permit(:building_type, :appartement, :floor, :basement, :plan, :business, :parking, :cages, :occupant, :elevatorNeeded, :price, :fees, :totalPrice)
+  end
+  
 
 end

b013aa836192466445b461165f44684894286c9f
coctusmccoy <juliendarac@gmail.com>
2021-11-05 18:31:30 -0400

    add admin privilege

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_05_164233) do
+ActiveRecord::Schema.define(version: 2021_11_05_215750) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "address"
@@ -215,6 +215,9 @@ ActiveRecord::Schema.define(version: 2021_11_05_164233) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.boolean "admin", default: false
+    t.boolean "employee", default: false
+    t.boolean "client", default: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end

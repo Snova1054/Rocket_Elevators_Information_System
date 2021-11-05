db700b54f0af5c38f7785b0ea2fe533c3e02ad98
coctusmccoy <juliendarac@gmail.com>
2021-11-05 19:44:16 -0400

    default admin and employee added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
 
     def require_admin
         # depending on your auth, something like...
-        redirect_to main_app.root_path unless current_user && current_user.admin?
+        redirect_to main_app.root_path unless current_user && current_user.admin? || current_user.employee?
     end
 
 end
